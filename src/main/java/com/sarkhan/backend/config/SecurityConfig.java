e7daf8fe1409fcea7e9088094b4113acc3b8f906
SarKhann <serxan.babayev.06@gmail.com>
2025-03-11 17:12:25 +0400

    Google Config

--- src/main/java/com/sarkhan/backend/config/SecurityConfig.java
+++ src/main/java/com/sarkhan/backend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.sarkhan.backend.config;
 
+import com.sarkhan.backend.model.user.CustomOAuth2User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,29 +9,37 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-    private final AuthenticationProvider authenticationProvider;
-
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/public/**","/oauth2/authorization/*").permitAll()
-                        .requestMatchers("/api/auth/**").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/public/**","/swagger-ui/**","/v3/api-docs/**","/swagger-ui.html").permitAll() // Herkese açık URL'ler
+                        .anyRequest().authenticated() // Geri kalan her şey kimlik doğrulama gerektirir
+                )
+                .oauth2Login(oauth2 -> oauth2 // OAuth2 kimlik doğrulamasını etkinleştir
+                        .defaultSuccessUrl("/home", true) // Giriş başarılı olduğunda yönlendirilecek sayfa
+
+                        .successHandler((request, response, authentication) -> {
+                                CustomOAuth2User customUser = (CustomOAuth2User) authentication.getPrincipal();
+
+                                response.setHeader("Authorization", "Bearer " + customUser.getJwtToken());
+                        })
                 );
+
         return http.build();
     }
-
-
 }
-
